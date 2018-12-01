7c6e387c9b812d61851a42e5682d7efeeff2b648
Trudelutt <tjostad@hotmail.com>
2018-12-01 16:20:18 +0100

    clean up code

--- train.py
+++ train.py
@@ -12,7 +12,6 @@ import tensorflow as tf
 from keras.backend.tensorflow_backend import set_session
 from metric import *
 from dice_coefficient_loss import dice_coefficient_loss
-from pickle import dump, load
 
 
 def gpu_config():
@@ -22,28 +21,6 @@ def gpu_config():
     #set_session(tf.Session(config = config))
     sess = tf.Session(config=config)
     sess.run(tf.global_variables_initializer())
-
-
-
-
-"""def write_png(path, array):
-    #new_array = array.reshape(512,256)
-    scipy.misc.imsave(path, array)
-
-def visulize_predic(p):
-    new_p = np.zeros((p.shape[0], p.shape[1]))
-    print("NEW P")
-    print(new_p.shape)
-    for i in range(new_p.shape[0]):
-        for j in range(new_p.shape[1]):
-            if(p[i][j][0] < p[i][j][1]):
-                new_p[i][j] = 1
-    return new_p
-
-def write_predictions_to_pngfile(p, target, path):
-    print("Writing predictions to file...")
-    write_png(path, target[0][...,0])
-    write_png("./predictions/prediction20.png", p[0][...,0])"""
 
 
 def plot_history(history, name, save=False):
@@ -73,26 +50,23 @@ def plot_history(history, name, save=False):
 
 def train_model(model, input, target, val_x, val_y, modelpath):
     print("Inside training")
-    print("Training sample" + str(input.shape))
 
     model_checkpoint = ModelCheckpoint("./models/"+ modelpath +".hdf5", monitor='val_loss',verbose=1, save_best_only=True)
     model_earlyStopp = EarlyStopping(monitor='val_loss', min_delta=0, patience=7, verbose=1, mode='min', baseline=None, restore_best_weights=False)
-    history = model.fit(x=input, y= target, validation_data=(val_x, val_y), batch_size=1, epochs=500, verbose=1, callbacks=[model_checkpoint, model_earlyStopp, TerminateOnNaN()])
+    history = model.fit(x=input, y= target, validation_data=(val_x, val_y), batch_size=1, epochs=1, verbose=1, callbacks=[model_checkpoint, model_earlyStopp, TerminateOnNaN()])
     with open('./history/'+ modelpath + '.json', 'w') as f:
-        json.dump(hist.history, f)
+        json.dump(history.history, f)
 
 def predict_model(model, input, target, name='LM_01'):
     print("Starting predictions")
-    p = model.predict(input)
-    print("prediction: " + str(p))
-    print(p.shape, target.shape)
+    p = model.predict(input,  batch_size=1, verbose=1)
     write_pridiction_to_file(target,p, path="./predictions/" + name + "prediction.nii.gz")
 
 
 def evaluation(model, test_files):
     test_x, test_y = get_train_data_slices(train_files)
     print("Starting evaluation.....")
-    print(model.evaluate(test_x, test_y))
+    print(model.evaluate(test_x, test_y), batch_size=1, verbose=1)
     print("Evaluation done..")
 
 
@@ -102,6 +76,8 @@ if __name__ == "__main__":
     model_name = "BVNet"
     label = "LM"
     modelpath = model_name+ "_"+ label
+    custom_objects = custom_objects={ 'binary_accuracy':binary_accuracy, 'recall':recall,
+    'precision':precision, 'dsc': dsc, 'dice_coefficient_loss': dice_coefficient_loss}
     if model_name == "BVNet":
         model = BVNet()
     else:
@@ -114,16 +90,12 @@ if __name__ == "__main__":
     val_data, val_label = get_slices(val_files)
     print("Done geting validation slices...")
 
-
     if  not overwrite:
-        prediction_model= load_model(modelpath +'.hdf5', custom_objects={'mean_iou': mean_iou, 'binary_accuracy':binary_accuracy, 'recall':recall,
-        'precision':precision, 'dsc': dsc, 'dice_coefficient_loss': dice_coefficient_loss})
+        prediction_model= load_model(modelpath +'.hdf5', custom_objects=custom_objects)
     else:
         train_model(model, train_data, label_data, val_data, val_label, modelpath=modelpath)
-        prediction_model = load_model(modelpath +'.hdf5', custom_objects={'binary_accuracy':binary_accuracy, 'recall':recall,
-        'precision':precision, 'dsc': dsc, 'dice_coefficient_loss': dice_coefficient_loss})
+        prediction_model = load_model(modelpath +'.hdf5', custom_objects=custom_objects)
     for i in range(len(test_files)):
         pred_sample, pred_label = get_prediced_image_of_test_files(test_files, i)
-        #print(pred_sample)
-        predict_model(prediction_model, pred_sample, pred_sample, name=model_path+"_"+i+"_")
+        predict_model(prediction_model, pred_sample, pred_sample, name=modelpath+"_"+str(i)+"_")
     evaluation(prediction_model, test_files)
