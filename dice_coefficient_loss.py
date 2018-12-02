34daccf3e21852d203e9d35b443a95d5ea326cdd
Trudelutt <tjostad@hotmail.com>
2018-12-02 23:09:58 +0100

    remove the slices with only background and add smooth factor

--- dice_coefficient_loss.py
+++ dice_coefficient_loss.py
@@ -7,7 +7,7 @@ def dice_coefficient(y_true, y_pred, smooth=1.):
     y_true_f = K.flatten(y_true)
     y_pred_f = K.flatten(y_pred)
     intersection = K.sum(y_true_f * y_pred_f)
-    return (2. * intersection) / (K.sum(y_true_f) + K.sum(y_pred_f) + K.epsilon())
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
 
 
 def dice_coefficient_loss(y_true, y_pred):
--- preprossesing.py
+++ preprossesing.py
@@ -37,7 +37,7 @@ def remove_slices_with_just_background(image, label):
             if(i < first_non_backgroud_slice):
                 first_non_backgroud_slice = i
             last_non_backgroud_slice = i
-    resize_label =  label[first_non_backgroud_slice - 2 - 2 :last_non_backgroud_slice + 1 + 2 + 2]
+    resize_label =  label[first_non_backgroud_slice - 2 :last_non_backgroud_slice + 1  + 2]
     resize_image =  image[first_non_backgroud_slice-2:last_non_backgroud_slice+1+2]
     return resize_image, resize_label
 
